da78eef00e3af4176468fa5887618aa4ee204411
Eve <Eve@EvenoMacBook-Pro.local>
2019-09-29 15:53:11 +0100

    washing hands audio

--- Assets/Scripts/Script/Objects/Sink.cs
+++ Assets/Scripts/Script/Objects/Sink.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Objects.MyObject;
+using UnityEngine;
 
-public class Sink : MyObject
+public class Sink : MonoBehaviour
 {
     string name = "Sink";
 
-    // Update is called once per frame
-    public override void react()
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    // Start is called before the first frame update
+    void Start()
     {
+        MusicSource.clip = MusicClip;
+
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        MusicSource.Play();
         print("I feel so dizzy....... Ehh the icy water makes me feel better.");
     }
+
+    void Update()
+    {
+        if (MusicSource.time > 3f)
+        {
+            MusicSource.Stop();
+        }
+    }
+
 }
